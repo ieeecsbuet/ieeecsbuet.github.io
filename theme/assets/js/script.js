1f566b572925753f50ed17c506e2e4404ce6b27d
fazledyn <1705066@ugrad.cse.buet.ac.bd>
2021-07-14 22:17:03 +0600

    JS, Fonts etc fat cleaned

--- theme/assets/js/script.js
+++ theme/assets/js/script.js
@@ -1,27 +1,6 @@
 (function ($) {
   'use strict';
 
-  // Preloader js    
-  $(window).on('load', function () {
-    $('.preloader').fadeOut(100);
-  });
-
-  // Sticky Menu
-  $(window).scroll(function () {
-    var height = $('.top-header').innerHeight();
-    if ($('header').offset().top > 10) {
-      $('.top-header').addClass('hide');
-      $('.navigation').addClass('nav-bg');
-      $('.navigation').css('margin-top','-'+height+'px');
-    } else {
-      $('.top-header').removeClass('hide');
-      $('.navigation').removeClass('nav-bg');
-      $('.navigation').css('margin-top','-'+0+'px');
-    }
-  });
-
-  
-
   // Background-images
   $('[data-background]').each(function () {
     $(this).css({
